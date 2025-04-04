ca50954b42c69aa7441f15f8ada92431fec0fe3b
Mikhail Pertsev <mpl4@Mikhails-MacBook-Pro.local>
2025-04-04 15:55:34 +0200

    initial commit

---
+++ src/backend/config.ts
@@ -0,0 +1,14 @@
+
+export const CONFIG = {
+    SHOW_VISUALIZATION: true, // ?
+    PLAY_SOUND: true,
+    LOAD_FROM_FILE: true,
+    SEND_REQUESTS: false, // переключить для отправки запросов
+    DEBUG: false,
+    LOGS_LIMIT: 100,
+    TOKEN: 'НАШ ТОКЕН',
+    BASE_URL: 'https://games-test.datsteam.dev',
+    GAME_ENDPOINT: '/play/magcarp/player/move', // поменять
+    STATUS_ENDPOINT: '/rounds/magcarp', // поменять
+    GO_AWAY_ERROR: 'GOAWAY' // поменять
+} as const;
